57a57ae6dafc91aaac94f301552cb5e0f44fe203
mshukor <mustafa.shukor97@gmail.com>
2021-02-26 12:36:21 +0100

    optimize more

--- kernels_for_sequence.py
+++ kernels_for_sequence.py
@@ -133,26 +133,34 @@ def substring_mismatch_kernel_fast(X1, X2, n=3, k=1, charset='ATCG'):
         return 0
     else:
         # Initialize counting dictionnaries
-        counts_min = {idx: {perm: 0 for perm in all_patterns} for idx in range(len(min_X))}
-        counts_max = {idx: {perm: 0 for perm in all_patterns} for idx in range(len(max_X))}
+        # counts_min = {idx: {perm: 0 for perm in all_patterns} for idx in range(len(min_X))}
+        # counts_max = {idx: {perm: 0 for perm in all_patterns} for idx in range(len(max_X))}
+
+        counts_min = {}
+        counts_max = {}
         # Iterate over sequences and count mers occurences
         for idx, (seq1, seq2) in tqdm(enumerate(zip(min_X, max_X)), disable=True):
+            c_min = {perm: 0 for perm in all_patterns}
+            c_max = c_min.copy()
             for i in range(seq_max_len - n):
                 subseq1 = get_tuple(seq1, i, n)
-                counts_min[idx] = count_pattern_mismatch(subseq1, counts_min[idx], neighbors)
+                c_min = count_pattern_mismatch(subseq1, c_min, neighbors)
                 subseq2 = get_tuple(seq2, i, n)
-                counts_max[idx] = count_pattern_mismatch(subseq2, counts_max[idx], neighbors)
+                c_max = count_pattern_mismatch(subseq2, c_max, neighbors)
 
-            counts_min[idx] = sparse.csr_matrix(np.fromiter(counts_min[idx].values(), dtype=np.float32))
-            counts_max[idx] = sparse.csr_matrix(np.fromiter(counts_max[idx].values(), dtype=np.float32))
+            counts_min[idx] = sparse.csr_matrix(np.fromiter(c_min.values(), dtype=np.float32))
+            counts_max[idx] = sparse.csr_matrix(np.fromiter(c_max.values(), dtype=np.float32))
 
         # Complete iteration over larger datasets
         for idx, seq in tqdm(enumerate(max_X[min_len:]), disable=True):
+            c_max = {perm: 0 for perm in all_patterns}
             for i in range(seq_max_len - n):
                 subseq = get_tuple(seq, i, n)
-                counts_max[idx + min_len] = count_pattern_mismatch(subseq, counts_max[idx + min_len], neighbors)
+                c_max = count_pattern_mismatch(subseq2, c_max, neighbors)
 
-            counts_max[idx + min_len] = sparse.csr_matrix(np.fromiter(counts_max[idx + min_len].values(), dtype=np.float32))
+                # counts_max[idx + min_len] = count_pattern_mismatch(subseq, counts_max[idx + min_len], neighbors)
+
+            counts_max[idx + min_len] = sparse.csr_matrix(np.fromiter(c_max.values(), dtype=np.float32))
 
         # Compute normalized inner product between spectral features
         feats1 = np.array([foo.A for foo in counts_max.values()]).squeeze()
